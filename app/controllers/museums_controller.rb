a6cc1f6f7442db495950b95b01d595a22e36c944
Edward Beesley <edwardjbeesley@gmail.com>
2022-07-14 16:52:51 +0100

    site now returns list of museums near the long and lat searched for

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -4,13 +4,14 @@ require "ostruct"
 class MuseumsController < ApplicationController
   def index
     # raise
-    @museum_data = get_api_data(params[:longitude], params[:latitude])
+    @museums = get_api_data(params[:longitude], params[:latitude])
   end
 
   private
 
   def get_api_data(longitude, latitude)
     url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=#{longitude},#{latitude}&access_token=#{ENV['MAPBOX_API_KEY']}"
+
     serialized = URI.parse(url).open.read
     map_data = JSON.parse(serialized, object_class: OpenStruct)
     map_data.features
